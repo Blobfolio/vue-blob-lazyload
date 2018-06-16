ab19e0f64910896bb3448cf131922ff655f6de3a
Josh Stoik <josh@blobfolio.com>
2018-06-16 12:16:01 -0700

    replace grunt with npm scripts

---
+++ npm_scripts/js-uglify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# NPM: Uglify Tasks
+#
+# These are a little too cumbersome to deal with inside NPM.
+##
+
+
+
+# Check dependencies.
+command -v uglifyjs >/dev/null 2>&1 || {
+	echo -e "\033[31;1mError:\033[0m uglifyjs must be in \$PATH."
+	echo -e "\033[96;1mFix:\033[0m npm i uglify-es -g"
+	exit 1
+}
+
+
+
+# Just one file.
+uglifyjs -c --ecma 6 -o "vue-what-goes-around.min.js" -- "src/js/vue-what-goes-around.js"
+echo -e "\033[2mminifying:\033[0m vue-what-goes-around.min.js"
+
+
+
+# We're done!
+echo -e "\033[32;1mSuccess:\033[0m Uglification has completed!"
+exit 0
